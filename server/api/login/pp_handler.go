65963050b7ef0b28e7d66a7218532e112b6beca5
ejean <esteban-jean.pro@hotmail.com>
2024-10-07 17:59:11 +0200

    Backend functions for user management

---
+++ server/api/login/pp_handler.go
@@ -0,0 +1,34 @@
+package authentification
+
+import (
+	"encoding/json"
+	"fmt"
+	dbUser "forum/server/users"
+	"net/http"
+	"os"
+)
+
+func PP_Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var usr dbUser.User
+
+	err := json.NewDecoder(r.Body).Decode(&usr)
+	if err != nil {
+		http.Error(w, "Fatal error decode id", http.StatusInternalServerError)
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	if usr.ProfilePicture, err = dbUser.FetchPPByID(usr.UUID); err != nil {
+		http.Error(w, "Fatal error query pp", http.StatusInternalServerError)
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	if usr.ProfilePicture == "" {
+		fmt.Printf("User not found for ID \"%s\"\n", usr.UUID)
+	}
+
+	json.NewEncoder(w).Encode(usr.ProfilePicture)
+}
